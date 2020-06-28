c8c71d8b2528ece83bb2493ce4d2faac5f75c1a9
Bryan Pero <bryanpero@gmail.com>
2020-06-28 19:29:57 -0400

    fixed syntax error in seeds file

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id int AUTO_INCREMENT NOT NULL,
+  burger_name varchar(255) NOT NULL,
+  devoured boolean,
+  PRIMARY KEY(id)
+);
