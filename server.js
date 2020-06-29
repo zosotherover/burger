89366524b01d9a276836603e75997997e6d2a7be
Bryan Pero <bryanpero@gmail.com>
2020-06-28 21:43:55 -0400

    worked on server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-const express = require(express);
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log("App now listening at localhost:" + PORT);
+});
