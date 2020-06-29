c80b84d151d588c6f31eaa21f8293e0455cf183c
Bryan Pero <bryanpero@gmail.com>
2020-06-28 22:48:37 -0400

    updated schema for Heroku/JAWSDB

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  id int AUTO_INCREMENT NOT NULL,
-  burger_name varchar(255) NOT NULL,
-  devoured boolean,
-  PRIMARY KEY(id)
+    id INTEGER AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT 0,
+    PRIMARY KEY (id)
 );
